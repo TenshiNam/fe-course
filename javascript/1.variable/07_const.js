00db3d99a1cfbb637935ca754d2a7c8930bb046e
Nam <tenshichan900@gmail.com>
2026-03-19 18:16:33 +0900

    2026-03-19 commit

---
+++ javascript/1.variable/07_const.js
@@ -0,0 +1,13 @@
+/**
+ * const : 상수 선언 시 사용
+ * - 정의 된 상수에 데이터 재할당 X
+ */
+
+const name = '홍길동';
+// name = '이순신'; // TypeError: Assignment to constant variable
+
+console.log(name);
+
+let name2 = '홍길동';
+name2 = '이순신';
+console.log(name2);
